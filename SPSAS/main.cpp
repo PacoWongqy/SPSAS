64b637e5e033ed0d7d89982f5285f0c8dc3d1495
Paco <125715654+PacoWongqy@users.noreply.github.com>
2026-09-26 15:39:02 +0800

    Add files via upload

--- SPSAS/main.cpp
+++ SPSAS/main.cpp
@@ -11,10 +11,34 @@ int main(int argc, char** argv) {
 		if (strcmp(ctrl, "E") == 0) {
 			return 0;
 		} else if ((strcmp(ctrl, "create") == 0) || strcmp(ctrl, "C") == 0) {
-	
-		} else if ((strcmp(ctrl, "read") == 0) || strcmp(ctrl, "R") == 0) {
+			system("cls");
+			char name[99]="0";
+			int line=0;
+			cout<<"line:"<<endl;
+			while(line!=-1){
+				cout<<">>";
+				cin>>line;
+				if(line==-1){
+					system("cls");
+					cout << "/SPSAS:create/read/write/E" << endl;
+					break;
+				}else{
+					cin>>name;
+					string path = "save/";
+					path += name;
+					ofstream ofs(path);
+					if (!ofs) {
+						cerr << "ERROR";
+						continue;
+					}
+					
+					ofs.close();
+				}
+			}
+		} else if ((strcmp(ctrl, "read") == 0) || strcmp(ctrl, "R") == 0) { //读取功能
 			system("cls");
 			cout << "list:" << endl;
+			cout << "save/" << endl;
 			string dirPath = "save";   // 相对于当前工作目录的 save 文件夹
 			if (!fs::exists(dirPath) || !fs::is_directory(dirPath)) {
 				std::cerr << "ERROR" << dirPath << std::endl;
@@ -23,7 +47,7 @@ int main(int argc, char** argv) {
 			for (const auto& entry : fs::directory_iterator(dirPath)) {
 				if (!entry.is_regular_file()) continue;          // 只看普通文件
 				if (entry.path().extension() == ".txt") {        // 扩展名是 .txt
-					std::cout << entry.path().filename().string() << std::endl;
+					cout << entry.path().filename().string() <<endl;
 				}
 			}
 			char name[99] = "0";
@@ -53,16 +77,30 @@ int main(int argc, char** argv) {
 					iss >> nuber;
 					for (int i = 0; i < nuber; ++i) {
 						if (!getline(fin, line)) {
-							cerr << "ERROR" <<endl;
+							cerr << "ERROR" << endl;
 							break;
 						}
 						if (!line.empty() && line.back() == '\r') {
 							line.pop_back();
 						};
-						for (size_t j = 0; j < line.size(); ++j) {
-							char ch = line[j];
-							//读
+						char ch = line[0];
+						int pu = 0, k = 1;
+						if (ch = '[') {
+							ch = line[k];
+							while (ch != ':') {
+								ch = line[k];
+								cout << ch;
+								k++;
+							}
 						}
+						for (int i = k; i <= line.size(); i++) {
+							if (line[i] == ',') {
+								cout << " ";
+							} else if ('0' <= line[i] && '9' >= line[i]) {
+								cout << line[i];
+							}
+						}
+						cout << endl;
 					}
 					fin.close();
 				}
